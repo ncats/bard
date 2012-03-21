6f49319db002cfd3aa15b93e44f114e5bb0ca46a
Rajarshi  Guha <rajarshi.guha@gmail.com>
2012-03-21 15:21:52 -0400

    Added another utility method

--- src/gov/nih/ncgc/bard/Util.java
+++ src/gov/nih/ncgc/bard/Util.java
@@ -1,5 +1,7 @@
 package gov.nih.ncgc.bard;
 
+import java.util.List;
+
 /**
  * Miscellaneous utility functions.
  *
@@ -13,6 +15,16 @@ public class Util {
         for (int i = 0; i < x.length; i++) {
             buffer.append(x[i]);
             if (i != x.length - 1) buffer.append(delim);
+        }
+        return buffer.toString();
+    }
+
+    public static String join(List<? extends Object> x, String delim) {
+        if (delim == null) delim = "";
+        StringBuffer buffer = new StringBuffer();
+        for (int i = 0; i < x.size(); i++) {
+            buffer.append(x.get(i));
+            if (i != x.size() - 1) buffer.append(delim);
         }
         return buffer.toString();
     }
