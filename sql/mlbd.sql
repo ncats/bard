9b3ed83d178fca4ff93878292c95e2e1da7e5f78
Rajarshi Guha <rajarshi.guha@gmail.com>
2012-03-21 15:06:59 -0400

    Initial structure for BARD related code. Currently contains SQL and code to generate Uniprot data for loading

---
+++ sql/mlbd.sql
@@ -0,0 +1,134 @@
+drop table assay_concentration;
+drop table assay_result;
+drop table assay_data;
+drop table assay_target;
+drop table assay_pub;
+drop table assay;
+drop table synonyms;
+drop table substance;
+drop table source_substance;
+drop table source;
+drop table compound;
+drop table protein_target;
+drop table publication;
+
+create table publication (
+    pmid number primary key,
+    title varchar2(4000),
+    abstract clob,
+    doi varchar2(256)
+    );
+
+create table protein_target (
+    accession varchar2(20) primary key,    
+    gene_id number,
+    name varchar2(4000),
+    taxid number,
+    description varchar2(4000),
+    uniprot_status varchar2(16)
+    );
+
+create table compound (
+    cid number primary key,
+    molfile clob,
+    creation date,
+    probe_id varchar2(128),
+    url varchar2(4000)
+    );
+
+create table substance (
+    sid number primary key,
+    cid number not null,
+    creation date,
+    constraint cid_fk foreign key (cid) references compound (cid)
+    );
+
+-- store synonyms for compounds, genes
+create table synonyms (
+    id number not null,
+    type number not null, -- 1 is CID, 2 is geneid
+    syn varchar2(4000) not null
+    );
+
+-- project management tables
+create table source (
+    source_id number primary key,
+    name varchar2(4000)
+    );
+create table source_substance (
+    source_id number primary key,
+    sid number not null,
+    constraint source_id_fk foreign key (source_id) references source (source_id)   
+    );
+
+-- assay related tables
+-- 
+-- the classification field in the MLBD schema seems a lot like a BAO annotation
+-- so I moved it out of assay and anticipate it goes into annotation. Similarly
+-- type could be moved to an annotation table
+create table assay (
+    aid number primary key,
+    name varchar2(4000),
+    description clob,
+    source_id number,
+    category number, -- mlscn (1), mlpcn (2), mlscn-ap (3), mlpcn-ap (4)
+    type number, -- other (0), screening (1), confirmatory (2),  summary (3)
+    summary number, -- parent summary AID (null if this assay is a summary assay)
+    grant_no varchar2(1024),
+    deposited date not null,
+    updated date,
+    constraint assay_src_fk foreign key (source_id) references source (source_id)
+    );
+
+create table assay_target (
+    aid number primary key,
+    accession varchar2(2) not null,
+    constraint aid_fk foreign key (aid) references assay (aid),
+    constraint acc_fk foreign key (accession) references protein_target (accession)
+    );
+
+create table assay_pub (
+    aid number primary key,
+    pmid number not null,
+    constraint ap_aid_fk foreign key (aid) references assay (aid),
+    constraint pmid_fk foreign key (pmid) references publication (pmid)
+    );    
+
+-- assay_data_id + updated lets us implement versioning
+-- A given aid+sid can have multiple entries, representing versions of a data point
+-- Replicate information for a given assay_data_id is contained in assay_result
+create table assay_data (
+    assay_data_id number primary key,
+    aid number not null,
+    sid number not null,
+    cid number not null,
+    classification number, -- substance acquisition classification: mlsmr (null or 0), purchased (1), synthesized (2)
+    updated date,
+
+    constraint ad_aid_fk foreign key (aid) references assay (aid),
+    constraint ad_cid_fk foreign key (cid) references compound (cid),
+    constraint ad_sid_fk foreign key (sid) references substance (sid)
+    );
+
+create table assay_result (
+    assay_data_id number,
+    replicate_id number default 1,
+    potency float,
+    outcome number,
+    score number,
+    s0 float, 
+    sInf float,
+    hill float,
+    lac50 float,
+
+    constraint ar_adi_fk foreign key (assay_data_id) references assay_data (assay_data_id)
+    );
+
+create table assay_concentration (
+    assay_data_id number,
+    concentration float,
+    response float,
+    conc_order number default 1,
+
+    constraint ac_adi_fk foreign key (assay_data_id) references assay_data (assay_data_id)
+    );
---
+++ src/gov/nih/ncgc/bard/tools/PopulateTargets.java
@@ -0,0 +1,93 @@
+package gov.nih.ncgc.bard.tools;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.util.zip.GZIPInputStream;
+
+/**
+ * A one line summary.
+ *
+ * @author Rajarshi Guha
+ */
+public class PopulateTargets {
+
+    String ofilename = "uniprot.sql";
+
+    public PopulateTargets(String ofilename) {
+        if (ofilename != null)
+            this.ofilename = ofilename;
+    }
+
+    public void run() throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter("uniprot.sql"));
+        writer.write("load data\n" +
+                "infile *\n" +
+                "append\n" +
+                "into table protein_target\n" +
+                "fields terminated by '\\t'\n" +
+                "trailing nullcols\n" +
+                "(accession, gene_id, name, tax id, description, uniprot_status)\n" +
+                "begindata\n");
+
+        URL uniprot = new URL("ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.dat.gz");
+        InputStream gzipStream = new GZIPInputStream(uniprot.openStream());
+        Reader reader = new InputStreamReader(gzipStream);
+        BufferedReader breader = new BufferedReader(reader);
+        String line = null;
+        StringBuffer sb = new StringBuffer();
+        int n = 0;
+        while ((line = breader.readLine()) != null) {
+            if (line.trim().equals("//")) {
+                String acc = "", status = "", name = "", geneid = "", desc = "", taxid = "";
+                String[] toks = sb.toString().split("\n");
+                for (String aline : toks) {
+                    if (aline.startsWith("ID")) {
+                        status = aline.split("\\s+")[2].replace(";", "");
+                    } else if (aline.startsWith("AC")) {
+                        acc = aline.split(";")[0].trim().replace("AC   ", "");
+                    } else if (aline.startsWith("DE   RecName:")) {
+                        name = aline.split("=")[1].replace(";", "");
+                    } else if (aline.startsWith("DR   GeneID;")) {
+                        geneid = aline.split(";")[1].trim();
+                    } else if (aline.startsWith("OX   ")) {
+                        taxid = aline.trim().replace("OX   NCBI_TaxID=", "").replace(";", "");
+                    }
+                }
+                writer.write(join(new String[]{acc, geneid, name, taxid, desc, status}, "\t") + "\n");
+                sb = new StringBuffer();
+
+                n++;
+                if (n % 100 == 0) System.out.print("\rProcessed " + n + " entries");
+            } else {
+                sb.append(line).append("\n");
+            }
+        }
+        System.out.println();
+    }
+
+    private static String join(Object[] x, String delim) {
+        if (delim == null) delim = "";
+        StringBuilder buffer = new StringBuilder();
+        for (int i = 0; i < x.length; i++) {
+            buffer.append(x[i]);
+            if (i != x.length - 1) buffer.append(delim);
+        }
+        return buffer.toString();
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        String ofilename = null;
+        if (args.length == 1) {
+            ofilename = args[0];
+        }
+        PopulateTargets pt = new PopulateTargets(ofilename);
+        pt.run();
+    }
+}
