c795d6209fc4925e306c86efd2ef5bcbdd3436ca
Rajarshi  Guha <rajarshi.guha@gmail.com>
2012-04-06 14:45:00 -0400

    Started adding utility methods to query the backend and return entity instances

---
+++ src/gov/nih/ncgc/bard/tools/DBUtils.java
@@ -0,0 +1,117 @@
+package gov.nih.ncgc.bard.tools;
+
+import gov.nih.ncgc.bard.entity.ProteinTarget;
+import gov.nih.ncgc.bard.entity.Publication;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.Reader;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Utility methods to interact with the database backend.
+ *
+ * @author Rajarshi Guha
+ */
+public class DBUtils {
+    Connection conn;
+
+    public DBUtils() {
+        conn = getConnection();
+    }
+
+    private Connection getConnection() {
+        javax.naming.Context initContext;
+        try {
+            initContext = new javax.naming.InitialContext();
+            javax.naming.Context envContext = (javax.naming.Context) initContext.lookup("java:/comp/env");
+            DataSource ds = (javax.sql.DataSource) envContext.lookup("jdbc/hcs");
+            return ds.getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private String readFromClob(Reader reader) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        if (reader == null) {
+            return null;
+        }
+        char[] buffer = new char[1];
+        while (reader.read(buffer) > 0) {
+            sb.append(buffer);
+        }
+        return sb.toString();
+    }
+
+    public Publication getPublicationByPmid(Long pmid) throws SQLException, IOException {
+        if (pmid == null) return null;
+        PreparedStatement pst = conn.prepareStatement("select * from publication where pmid = ?");
+        pst.setLong(1, pmid);
+        ResultSet rs = pst.executeQuery();
+        Publication p = new Publication();
+        while (rs.next()) {
+            p.setTitle(rs.getString("title"));
+            p.setDoi(rs.getString("doi"));
+            p.setPubmedId(pmid);
+            p.setAbs(readFromClob(rs.getCharacterStream("abstract")));
+        }
+
+        return p;
+    }
+
+    public Publication getPublicationByDoi(String doi) throws SQLException, IOException {
+        if (doi == null || doi.trim().equals("")) return null;
+        PreparedStatement pst = conn.prepareStatement("select * from publication where doi = ?");
+        pst.setString(1, doi);
+        ResultSet rs = pst.executeQuery();
+        Publication p = new Publication();
+        while (rs.next()) {
+            p.setTitle(rs.getString("title"));
+            p.setDoi(rs.getString("doi"));
+            p.setPubmedId(rs.getLong("pmid"));
+            p.setAbs(readFromClob(rs.getCharacterStream("abstract")));
+        }
+
+        return p;
+    }
+
+    public ProteinTarget getProteinTargetByAccession(String accession) throws SQLException {
+        if (accession == null || accession.trim().equals("")) return null;
+        PreparedStatement pst = conn.prepareStatement("select * from protein_target where accession  = ?");
+        pst.setString(1, accession);
+        ResultSet rs = pst.executeQuery();
+        ProteinTarget p = new ProteinTarget();
+        while (rs.next()) {
+            p.setAcc(accession);
+            p.setDescription(rs.getString("description"));
+            p.setGeneId(rs.getLong("geneid"));
+            p.setTaxId(rs.getLong("taxid"));
+            p.setName(rs.getString("name"));
+            p.setStatus(rs.getString("status"));
+        }
+        return p;
+    }
+
+    public ProteinTarget getProteinTargetByGeneid(Long geneId) throws SQLException {
+        if (geneId == null) return null;
+        PreparedStatement pst = conn.prepareStatement("select * from protein_target where geneid = ?");
+        pst.setLong(1, geneId);
+        ResultSet rs = pst.executeQuery();
+        ProteinTarget p = new ProteinTarget();
+        while (rs.next()) {
+            p.setAcc(rs.getString("accession"));
+            p.setDescription(rs.getString("description"));
+            p.setGeneId(rs.getLong("geneid"));
+            p.setTaxId(rs.getLong("taxid"));
+            p.setName(rs.getString("name"));
+            p.setStatus(rs.getString("status"));
+        }
+        return p;
+    }
+
+}
