79230f209e1835886952f148b284619c959f0c35
Rajarshi  Guha <rajarshi.guha@gmail.com>
2012-04-18 14:44:14 -0400

    Compound resource refactored to simplify code. Returns appropriately formatted molecules

---
+++ src/gov/nih/ncgc/bard/rest/RequestTooLargeException.java
@@ -0,0 +1,28 @@
+package gov.nih.ncgc.bard.rest;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+
+/**
+ * Provides an exception for HTTP 413 (Request Too Large)
+ *
+ * @author Rajarshi Guha
+ */
+public class RequestTooLargeException extends WebApplicationException {
+    /**
+     * Create a HTTP 413  exception.
+     */
+    public RequestTooLargeException() {
+        super(Response.status(413).build());
+    }
+
+    /**
+     * Create a HTTP 413 exception.
+     *
+     * @param message the String that is the entity of the 413 response.
+     */
+    public RequestTooLargeException(String message) {
+        super(Response.status(413).entity(message).type("text/plain").build());
+    }
+
+}
