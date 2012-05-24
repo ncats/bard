e3e158a5c578f1edab7cff7dca5025dbeb61957a
Rajarshi  Guha <rajarshi.guha@gmail.com>
2012-05-24 16:11:07 -0400

    some generic stuff for the _info resource

--- src/gov/nih/ncgc/bard/rest/BARDPluginRegistryResource.java
+++ src/gov/nih/ncgc/bard/rest/BARDPluginRegistryResource.java
@@ -1,17 +1,32 @@
 package gov.nih.ncgc.bard.rest;
 
+import gov.nih.ncgc.bard.tools.Util;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 /**
  * A one line summary.
  *
  * @author Rajarshi Guha
  */
+@Path("/v1/plugins/registry")
 public class BARDPluginRegistryResource implements IBARDResource {
+
+    @GET
+    @Produces("text/plain")
+    @Path("/_info")
     public String info() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        StringBuilder msg = new StringBuilder("Lists available plugins\n\nAvailable resources:\n");
+        List<String> paths = Util.getResourcePaths(this.getClass());
+        for (String path : paths) msg.append(path).append("\n");
+        msg.append("/v1/plguins/" + BARDConstants.API_EXTRA_PARAM_SPEC + "\n");
+        return msg.toString();
     }
 
     /**
@@ -24,6 +39,8 @@ public class BARDPluginRegistryResource implements IBARDResource {
         return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 
+    @GET
+    @Produces("application/json")
     public Response getResources(@QueryParam("filter") String filter, @QueryParam("expand") String expand, @QueryParam("skip") Integer skip, @QueryParam("top") Integer top) {
         return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
