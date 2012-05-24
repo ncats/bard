6d7937de4e2145a44b6673fcd7c807587458fa92
Rajarshi  Guha <rajarshi.guha@gmail.com>
2012-05-24 10:50:05 -0400

    Added example plugins. Updated build file

--- src/gov/nih/ncgc/bard/plugin/HelloWorldPlugin.java
+++ src/gov/nih/ncgc/bard/plugin/HelloWorldPlugin.java
@@ -1,27 +1,22 @@
 package gov.nih.ncgc.bard.plugin;
 
-import gov.nih.ncgc.bard.entity.Assay;
 import gov.nih.ncgc.bard.rest.MLBDConstants;
-import gov.nih.ncgc.bard.tools.DBUtils;
 import gov.nih.ncgc.bard.tools.Util;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.sql.SQLException;
 import java.util.List;
 
 /**
- * A BARD plugin implementing trivial assay related operations.
+ * The simplest possible plugin.
  *
  * @author Rajarshi Guha
  */
-@Path("/v1/plugins/ainfo")
-public class AssayInfoPlugin implements IPlugin {
+@Path("/v1/plugins/hworld")
+public class HelloWorldPlugin implements IPlugin {
     private static final String VERSION = "1.0";
 
     /**
@@ -33,35 +28,16 @@ public class AssayInfoPlugin implements IPlugin {
     @Produces("text/plain")
     @Path("/_info")
     public String getDescription() {
-        return "description of the plugin, including inputs, outputs and media types";
+        return "Trivial plugin that does nothing";
     }
 
     @GET
-    @Path("/{aid}/title")
-    public Response getAssayTitle(@PathParam("aid") Long aid) {
-        DBUtils db = new DBUtils();
-        try {
-            Assay assay = db.getAssayByAid(aid);
-            db.closeConnection();
-            return Response.ok(assay.getName(), MediaType.TEXT_PLAIN).build();
-        } catch (SQLException e) {
-            throw new WebApplicationException(500);
-        }
+    @Path("/aresource")
+    @Produces("text/html")
+    public Response getResource() {
+        return Response.ok("<b>Hello World</b>", MediaType.TEXT_HTML).build();
     }
 
-
-    @GET
-    @Path("/{aid}/description")
-    public Response getAssayDescription(@PathParam("aid") Long aid) {
-        DBUtils db = new DBUtils();
-        try {
-            Assay assay = db.getAssayByAid(aid);
-            db.closeConnection();
-            return Response.ok(assay.getDescription(), MediaType.TEXT_PLAIN).build();
-        } catch (SQLException e) {
-            throw new WebApplicationException(500);
-        }
-    }
 
     /**
      * Get the manifest for this plugin.
