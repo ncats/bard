3a0ba5c2a77a4ba29692f2d0e334ccfe20bfa5a6
Rajarshi  Guha <rajarshi.guha@gmail.com>
2012-05-24 10:05:50 -0400

    Got first draft if validator going

--- src/gov/nih/ncgc/bard/tools/PluginValidator.java
+++ src/gov/nih/ncgc/bard/tools/PluginValidator.java
@@ -5,13 +5,15 @@ import gov.nih.ncgc.bard.plugin.IPlugin;
 import gov.nih.ncgc.bard.rest.MLBDDocumentResource;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * r
  * A tool to validate BARD plugins.
  *
  * @author Rajarshi Guha
@@ -28,7 +30,7 @@ public class PluginValidator {
         return errors;
     }
 
-    public boolean validate(Class klass) {
+    public boolean validate(Class klass) throws IllegalAccessException, InstantiationException {
 
         // check appropriate interface
         boolean implementsInterface = false;
@@ -57,6 +59,23 @@ public class PluginValidator {
         if (!hasClassLevelPathAnnot)
             errors.add("Missing the class level @Path annotation or else the annotation did not start with '/v1/plugins/'");
 
+        Method[] methods = klass.getMethods();
+
+        // check for the _info resource
+        boolean infoResourcePresent = false;
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(Path.class)) {
+                Path annot = method.getAnnotation(Path.class);
+                // make sure the @GET annotation is present and the annotation is on the expected method
+                if (annot.value().equals("/_info") && method.getAnnotation(GET.class) != null && method.getName().equals("getDescription")) {
+                    infoResourcePresent = true;
+                    break;
+                }
+            }
+        }
+        if (!infoResourcePresent)
+            errors.add("Missing the getDescription() method with @Path(\"/_info\") and @GET annotations");
+
         // check that we have at least one (public) method that is annotated 
         // with a GET or a POST and has a non null @Path annotation
         // and a @Produces annotations
@@ -64,46 +83,42 @@ public class PluginValidator {
         // Note that this check excludes the method annotated with the _info 
         // resource path
         boolean resourcePresent = false;
-        boolean infoResourcePresent = false;
-        Method[] methods = klass.getMethods();
-
         for (Method method : methods) {
             if (method.isAnnotationPresent(Path.class)) {
                 Path annot = method.getAnnotation(Path.class);
-                // make sure the @GET annotation is present
-                if (annot.value().equals("/_info") && method.getAnnotation(GET.class) != null) {
-                    infoResourcePresent = true;
-                    break;
-                }
-            }
-        }
-        if (!infoResourcePresent) errors.add("Missing a method with @Path(\"info\") and @GET annotations");
+                if (annot.value().equals("/_info")) continue;
 
-        for (Method method : methods) {
-            if (method.isAnnotationPresent(Path.class)) {
-                Path annot = method.getAnnotation(Path.class);
-                if (!annot.value().equals("/_info")) {
-                    resourcePresent = true;
-                    break;
+                // check for a @GET/@POST/@PUT
+                if (method.isAnnotationPresent(GET.class) ||
+                        method.isAnnotationPresent(POST.class) ||
+                        method.isAnnotationPresent(PUT.class)) {
+                    // check for a @Produces
+                    if (method.isAnnotationPresent(Produces.class)) {
+                        resourcePresent = true;
+                        break;
+                    }
                 }
             }
         }
         if (!resourcePresent)
             errors.add("At least one public method must have a @Path annotation (in addition to the _info resource");
 
+        // ok, now we create the class
+        IPlugin plugin = (IPlugin) klass.newInstance();
 
-        // check for a non-null description, version
-        // check that we get back a manifest document
+        // check for a non-null description, version, manifest
+        String s = plugin.getDescription();
+        if (s == null) errors.add("getDescription() returned a null value");
+        s = plugin.getManifest();
+        if (s == null) errors.add("getManifest() returned a null value");
+        s = plugin.getVersion();
+        if (s == null) errors.add("getVersion() returned a null value");
+
         // validate the manifest document
-        // check for expected annotations and ensure that resource paths are properly defined
-        //  check for @GET/@POST
-        //  check for @Path (at least one must be present)
-        //  check for @Produces (at least one must be present for each method annotated with @GET/@POST)
-
         return errors.size() == 0;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InstantiationException, IllegalAccessException {
         PluginValidator v = new PluginValidator();
         CSLSPlugin p = new CSLSPlugin();
         MLBDDocumentResource d = new MLBDDocumentResource();
