0c693e9d304516489f3a8ffe3e45f2065079671f
Rajarshi  Guha <rajarshi.guha@gmail.com>
2012-06-12 12:24:15 -0400

    Fixed a typo

--- src/gov/nih/ncgc/bard/rest/BARDConstants.java
+++ src/gov/nih/ncgc/bard/rest/BARDConstants.java
@@ -14,7 +14,7 @@ public class BARDConstants {
 
     public static final String API_VERSION = "v1";
     public static final String API_BASE = "/bard/rest/" + API_VERSION;
-    public static final String API_EXTRA_PARAM_SPEC = "?search=query_string[field]&expand=true|false&skip=N&stop=M";
+    public static final String API_EXTRA_PARAM_SPEC = "?search=query_string[field]&expand=true|false&skip=N&top=M";
 
     /**
      * The maximum number of compounds to be returned by default.
