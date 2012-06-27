1ea5b8334c63ebefca8beaa9c9752636081b1704
Rajarshi  Guha <rajarshi.guha@gmail.com>
2012-06-27 11:35:26 -0400

    Added unti test to check that we get valid CIDs for probe ids

--- test/gov/nih/ncgc/bard/tools/DBUtilsTest.java
+++ test/gov/nih/ncgc/bard/tools/DBUtilsTest.java
@@ -2,6 +2,7 @@ package gov.nih.ncgc.bard.tools;
 
 import gov.nih.ncgc.bard.entity.Assay;
 import gov.nih.ncgc.bard.entity.BardEntity;
+import gov.nih.ncgc.bard.entity.Compound;
 import gov.nih.ncgc.bard.entity.ExperimentData;
 import gov.nih.ncgc.bard.entity.Substance;
 import org.testng.Assert;
@@ -73,5 +74,24 @@ public class DBUtilsTest extends DBTest {
         Substance s = db.getSubstanceBySid(sid);
         Assert.assertNotNull(s);
         Assert.assertFalse(s.getSmiles().equals(""));
+    }
+
+
+    @DataProvider
+    public Object[][] probeIdProvider() {
+        return new Object[][]{
+                {"ML099", new Long(888706)},
+                {"ML043", new Long(5401876)},
+                {"ML149", new Long(2331284)},
+                {"ML017", new Long(4131581)}
+        };
+    }
+
+    @Test(dataProvider = "probeIdProvider")
+    public void getCompoundByProbeId(String probeId, Long cid) throws SQLException {
+        Compound c = db.getCompoundByProbeId(probeId);
+        Assert.assertNotNull(c);
+        Assert.assertEquals(c.getCid(), cid);
+
     }
 }
