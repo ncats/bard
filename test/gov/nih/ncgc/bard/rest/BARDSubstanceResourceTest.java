cae760ad94fc7ba8d5fdf2b3fd486cd03d0a9036
Rajarshi  Guha <rajarshi.guha@gmail.com>
2012-07-09 11:00:54 -0400

    Added a test to check that we can deserialize a JSON substance record

--- test/gov/nih/ncgc/bard/rest/BARDSubstanceResourceTest.java
+++ test/gov/nih/ncgc/bard/rest/BARDSubstanceResourceTest.java
@@ -1,6 +1,15 @@
 package gov.nih.ncgc.bard.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import gov.nih.ncgc.bard.entity.Substance;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
 
 /**
  * A test of the substance resource.
@@ -18,4 +27,26 @@ public class BARDSubstanceResourceTest extends EntityResourceTest {
         setResourceName("substances");
     }
 
+
+    @Test
+    public void getCompoundByCid() throws IOException {
+        String url = prefix + resourceName + "/8144";
+        WebResource resource = client.resource(url);
+        ClientResponse response = resource.get(ClientResponse.class);
+        int status = response.getStatus();
+        Assert.assertEquals(status, 200, "Response was " + status + " rather than 200");
+
+        MediaType type = response.getType();
+        Assert.assertTrue(type.toString().equals("application/json"));
+
+        String json = response.getEntity(String.class);
+        Assert.assertNotNull(json);
+        Assert.assertTrue(!json.trim().equals(""));
+
+        Object o = new ObjectMapper().readValue(json, Substance.class);
+        Assert.assertTrue(o instanceof Substance);
+
+        Substance s = (Substance) o;
+        Assert.assertEquals(new Long(8144), s.getSid());
+    }
 }
