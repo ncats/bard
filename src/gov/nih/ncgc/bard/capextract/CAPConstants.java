240b7b2f62782b26270a1ea5bd213b61bdcccbc0
Rajarshi Guha <rajarshi.guha@gmail.com>
2012-07-11 00:21:01 -0400

    Got initial code to connect to BARD data export API and access the root element. To run it, you will need to secify the API via a system property

---
+++ src/gov/nih/ncgc/bard/capextract/CAPConstants.java
@@ -0,0 +1,51 @@
+package gov.nih.ncgc.bard.capextract;
+
+/**
+ * Useful constants for interacting with the CAP
+ *
+ * @author Rajarshi Guha
+ */
+public class CAPConstants {
+
+    public static final String CAP_ROOT = "https://bard.broadinstitute.org/dataExport/api";
+    public static final String CAP_ROOT_MIMETYPE = "application/vnd.bard.cap+xml;type=bardexport";
+    public static final String CAP_APIKEY_HEADER = "APIKEY";
+
+    /**
+     * Get the API key.
+     * <p/>
+     * This shoud be defined via a system property during startup. If using the command line
+     * use -DCAP_API_KEY=your-api-key. If using Tomcat, set it via an init-param.
+     *
+     * @return the API key required for all CAP API requests
+     */
+    public static String getApiKey() {
+        return System.getProperty("CAP_API_KEY");
+    }
+
+    public static enum CapResource {
+        DICTIONARY("application/vnd.bard.cap+xml;type=dictionary"),
+        RESULT_TYPE("application/vnd.bard.cap+xml;type=resultType"),
+        ELEMENT("application/vnd.bard.cap+xml;type=element"),
+        STAGE("application/vnd.bard.cap+xml;type=stage"),
+        ASSAYS("application/vnd.bard.cap+xml;type=assays"),
+        ASSAY("application/vnd.bard.cap+xml;type=assay"),
+        PROJECTS("application/vnd.bard.cap+xml;type=projects"),
+        PROJECT("application/vnd.bard.cap+xml;type=project"),
+        EXPERIMENTS("application/vnd.bard.cap+xml;type=experiments"),
+        EXPERIMENT("application/vnd.bard.cap+xml;type=experiment"),
+        RESULTS("application/vnd.bard.cap+xml;type=results"),
+        RESULT("application/vnd.bard.cap+xml;type=result");
+
+        private String mimeType;
+
+        private CapResource(String mimeType) {
+            this.mimeType = mimeType;
+        }
+
+        public String getMimeType() {
+            return mimeType;
+        }
+    }
+
+}
--- src/gov/nih/ncgc/bard/capextract/CAPExtractor.java
+++ src/gov/nih/ncgc/bard/capextract/CAPExtractor.java
@@ -1,12 +1,13 @@
 package gov.nih.ncgc.bard.capextract;
 
-import gov.nih.ncgc.bard.capextract.jaxb.DescriptorType;
-import gov.nih.ncgc.bard.capextract.jaxb.Dictionary;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.filter.LoggingFilter;
+import gov.nih.ncgc.bard.capextract.jaxb.Bardexport;
 
-import java.io.File;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Unmarshaller;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Example code to play with the Broad CAP Data Export API.
@@ -14,27 +15,30 @@ import javax.xml.bind.Unmarshaller;
  * @author Rajarshi Guha
  */
 public class CAPExtractor {
-    public static final String EXPORT_URL = "http://bard.nih.gov/bardexport";
 
     public CAPExtractor() {
     }
 
-    public boolean run() {
-        return true;
+    public void run() throws IOException, NoSuchAlgorithmException {
+        Client client = ClientHelper.createClient();
+        client.addFilter(new LoggingFilter());
+
+        WebResource resource = client.resource(CAPConstants.CAP_ROOT);
+        ClientResponse response = resource.accept(CAPConstants.CAP_ROOT_MIMETYPE).
+                header(CAPConstants.CAP_APIKEY_HEADER, CAPConstants.getApiKey()).
+                get(ClientResponse.class);
+
+        int status = response.getStatus();
+        if (status != 200) throw new IOException("Got HTTP " + status + " from data export API");
+
+        Bardexport s = response.getEntity(Bardexport.class);
+
     }
 
-    public static void main(String[] args) {
-//        CAPExtractor extractor = new CAPExtractor();
-//        extractor.run();
-
-	try {
-	    JAXBContext jc = JAXBContext.newInstance("gov.nih.ncgc.bard.capextract.jaxb");
-	    Unmarshaller unmarshaller = jc.createUnmarshaller();
-	    Dictionary dict = (Dictionary)unmarshaller.unmarshal(new File("resources/test/dictionary.xml"));
-	
-	    String yo = "";
-	    for (DescriptorType item: dict.getAssayDescriptors().getAssayDescriptor()) yo = yo + item.getLabel() + "\n";
-	    System.out.println(yo);
-	} catch (Exception ex) {ex.printStackTrace();}
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        CAPExtractor c = new CAPExtractor();
+        c.run();
     }
+
+
 }
