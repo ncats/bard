9e5c77ddc6a88323c8fce22a14e9230709fcafe3
Rajarshi Guha <rajarshi.guha@gmail.com>
2012-07-30 13:18:49 -0400

    Got the initial code to collect annotations and group them together. Actually dumping them to the db remains

--- src/gov/nih/ncgc/bard/capextract/handler/AssayHandler.java
+++ src/gov/nih/ncgc/bard/capextract/handler/AssayHandler.java
@@ -4,11 +4,13 @@ import gov.nih.ncgc.bard.capextract.CAPConstants;
 import gov.nih.ncgc.bard.capextract.ICapResourceHandler;
 import gov.nih.ncgc.bard.capextract.jaxb.Assay;
 import gov.nih.ncgc.bard.capextract.jaxb.AssayDocument;
-import gov.nih.ncgc.bard.capextract.jaxb.Link;
 
 import java.io.IOException;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * A one line summary.
@@ -56,23 +58,123 @@ public class AssayHandler extends CapResourceHandler implements ICapResourceHand
 
         /* This block extracts the annotations for the assay */
         List<Assay.MeasureContextItems.MeasureContextItem> mcis = null;
+        Map<String, CAPAssayAnnotation> annos = new HashMap<String, CAPAssayAnnotation>();
         if (assay.getMeasureContextItems() != null) {
             mcis = assay.getMeasureContextItems().getMeasureContextItem();
             for (Assay.MeasureContextItems.MeasureContextItem mci : mcis) {
-                String extId = mci.getExtValueId();
-                Assay.MeasureContextItems.MeasureContextItem.AttributeId attrid = mci.getAttributeId();
-                Link link = attrid.getLink();
-                if (link.getHref().contains("dataExport/api/dictionary"))
-                    System.out.println("key = [" + attrid.getLabel() + "," + attrid.getAttributeType() + "," + attrid.getLink().getHref() + "] [extid = " + extId + "] value = " + mci.getValueDisplay());
+
+                String valueid = null, attrid = null, attrtype = null;
+                String displayValue = mci.getValueDisplay();
+                BigInteger id = mci.getMeasureContextItemId();
+                BigInteger refid = mci.getMeasureContextItemRef(); // may be null                
+                String contextref = mci.getMeasureContextRef(); //  may be null
+
+                Assay.MeasureContextItems.MeasureContextItem.ValueId mcivid = mci.getValueId();
+                if (mcivid != null) valueid = mcivid.getLink().getHref();
+                Assay.MeasureContextItems.MeasureContextItem.AttributeId mciaid = mci.getAttributeId();
+                if (mciaid != null) attrid = mciaid.getLink().getHref();
+                String extid = mci.getExtValueId();
+
+                CAPAssayAnnotation anno = new CAPAssayAnnotation(id.toString(), refid.toString(), displayValue, contextref, attrid, valueid, extid);
+                annos.put(id.toString(), anno);
+
+                System.out.println(anno);
             }
         }
+        System.out.println("------------");
+        log.info("\tGot " + annos.size() + " annotations");
 
+        // ok, we have a list of annotations, now we reconstruct the groups
+        Map<String, List<String>> annogrps = new HashMap<String, List<String>>();
+        for (CAPAssayAnnotation anno : annos.values()) {
+            String id = anno.id;
+            String refid = anno.refId;
+
+            if (refid.equals(id)) refid = null;
+
+            if (refid != null) {
+                if (annogrps.containsKey(refid)) {
+                    List<String> tmp = annogrps.get(refid);
+                    tmp.add(id);
+                    annogrps.put(refid, tmp);
+                } else { //  if we have a null refid, just add the id itself to the list of related ids
+                    List<String> tmp = new ArrayList<String>();
+                    tmp.add(id);
+                    annogrps.put(refid, tmp);
+                }
+            } else {
+                if (!annogrps.containsKey(id)) annogrps.put(id, new ArrayList<String>());
+            }
+        }
+        log.info("\tReconstructed annotation groups and got " + annogrps.size() + " groups");
+        for (String key : annogrps.keySet()) {
+            System.out.print(key + ": ");
+            for (String s : annogrps.get(key)) System.out.print(s + " ");
+            System.out.println("\n");
+        }
+        System.out.println("----------");
+
+
+        // at this stage we have a list of annotations and we have groups of annotations
+        // as we write each annotation to the db, we want to list the other annotations in
+        // its group (if any). This is obviously repetitive but lets us access related 
+        // annots given any annot
+        List<List<String>> flatgrps = new ArrayList<List<String>>();
+        for (String key : annogrps.keySet()) {
+            List<String> tmp = new ArrayList<String>();
+            tmp.add(key);
+            tmp.addAll(annogrps.get(key));
+            flatgrps.add(tmp);
+        }
+        for (List<String> ls : flatgrps) {
+            for (String s : ls) System.out.print(s + " ");
+            System.out.println();
+        }
+        log.info("\tFlattened annotations into " + flatgrps.size() + " groups");
+
+        // at this point we can dump annos to the db. Importantly, we store annotations
+        // such that instead of the MeasureContextItemId identifiers, we resolve the
+        // annotations to sets of dictionary identifiers. This means when recording related
+        // annotations, we actually record related dictionary identifiers
         /* looking at measures */
         Assay.Measures measures = assay.getMeasures();
         if (measures != null) {
             for (Assay.Measures.Measure measure : measures.getMeasure()) {
                 System.out.println("measure.getMeasureContextRef() = " + measure.getMeasureContextRef());
             }
+        }
+    }
+
+
+    static class CAPAssayAnnotation {
+        String id, refId = null, display;
+        String contextRef = null;
+        String attrId, valueId; // refers to a dict element
+        String extValueId = null; // when dict element points to ext resource (e.g. Entrez Gene) this is the identifier within that resource
+
+        CAPAssayAnnotation(String id, String refId, String display, String contextRef, String attrId, String valueId, String extValueId) {
+            this.id = id;
+            this.refId = refId;
+            this.display = display;
+            this.contextRef = contextRef;
+            this.attrId = attrId;
+            this.valueId = valueId;
+            this.extValueId = extValueId;
+        }
+
+        CAPAssayAnnotation() {
+        }
+
+        @Override
+        public String toString() {
+            return "CAPAssayAnnotation{" +
+                    "id='" + id + '\'' +
+                    ", refId='" + refId + '\'' +
+                    ", display='" + display + '\'' +
+                    ", contextRef='" + contextRef + '\'' +
+                    ", attrId='" + attrId + '\'' +
+                    ", valueId='" + valueId + '\'' +
+                    '}';
         }
     }
 }
