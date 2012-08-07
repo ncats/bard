ca95f60a03fede3bf1bc0ab75b3733ea3cdc54a2
Rajarshi Guha <rajarshi.guha@gmail.com>
2012-08-07 11:23:15 -0400

    Added a resource to expose the CAP dictionary, allowing for look up of element details

---
+++ src/gov/nih/ncgc/bard/rest/BARDCapResource.java
@@ -0,0 +1,55 @@
+package gov.nih.ncgc.bard.rest;
+
+import gov.nih.ncgc.bard.capextract.CAPDictionary;
+import gov.nih.ncgc.bard.tools.DBUtils;
+import gov.nih.ncgc.bard.tools.Util;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ * A resource to expose CAP information.
+ * <p/>
+ * Currently the this resource only has one subresource that exposes
+ * the CAP dictionary. This is is exposed a direct conversion from the
+ * internal data structure ({@link CAPDictionary}) to a JSON representation,
+ * so it could be optimized in the future.
+ *
+ * @author Rajarshi Guha
+ */
+@Path("/cap")
+public class BARDCapResource implements IBARDResource {
+    public String info() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @GET
+    @Path("/dictionary")
+    public Response getResources(@QueryParam("filter") String filter, @QueryParam("expand") String expand, @QueryParam("skip") Integer skip, @QueryParam("top") Integer top) {
+        DBUtils db = new DBUtils();
+        try {
+            CAPDictionary dict = db.getCAPDictionary();
+            String json = Util.toJson(dict);
+            db.closeConnection();
+            return Response.ok(json).type(MediaType.APPLICATION_JSON).build();
+        } catch (SQLException e) {
+            throw new WebApplicationException(e, 500);
+        } catch (IOException e) {
+            throw new WebApplicationException(e, 500);
+        } catch (ClassNotFoundException e) {
+            throw new WebApplicationException(e, 500);
+        }
+
+    }
+
+    public Response getResources(@PathParam("name") String resourceId, @QueryParam("filter") String filter, @QueryParam("expand") String expand) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
