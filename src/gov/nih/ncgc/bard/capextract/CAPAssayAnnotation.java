6485f725b2b36f999dee756d942c2d3406220af3
Rajarshi Guha <rajarshi.guha@gmail.com>
2012-08-07 07:56:50 -0400

    Some minor refactoring and added Javadocs

--- src/gov/nih/ncgc/bard/capextract/CAPAssayAnnotation.java
+++ src/gov/nih/ncgc/bard/capextract/CAPAssayAnnotation.java
@@ -3,7 +3,11 @@ package gov.nih.ncgc.bard.capextract;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 /**
- * A one line summary.
+ * A representation of an annotation.
+ * <p/>
+ * This is class is generic and meant to handle arbitrary annotations on arbitrary entities. Currently
+ * it is primarily derived from the CAP Assay annotations, but in the future will probably be modified
+ * to support non-CAP annotations.
  *
  * @author Rajarshi Guha
  */
@@ -15,7 +19,7 @@ public class CAPAssayAnnotation {
     public String id;
     public String display;
     public String contextRef = null;
-    public String attrId, valueId; // refers to a dict element
+    public String key, value; // refers to a dict element
     public String extValueId = null; // when dict element points to ext resource (e.g. Entrez Gene) this is the identifier within that resource
 
     public CAPAssayAnnotation(String id, String refId, String display, String contextRef, String attrId, String valueId, String extValueId, String source) {
@@ -24,8 +28,8 @@ public class CAPAssayAnnotation {
 
         this.display = display;
         this.contextRef = contextRef;
-        this.attrId = attrId;
-        this.valueId = valueId;
+        this.key = attrId;
+        this.value = valueId;
         this.extValueId = extValueId;
         this.source = source;
     }
@@ -40,8 +44,8 @@ public class CAPAssayAnnotation {
                 ", refId='" + refId + '\'' +
                 ", display='" + display + '\'' +
                 ", contextRef='" + contextRef + '\'' +
-                ", attrId='" + attrId + '\'' +
-                ", valueId='" + valueId + '\'' +
+                ", key='" + key + '\'' +
+                ", value='" + value + '\'' +
                 '}';
     }
 }
