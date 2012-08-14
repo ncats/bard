30b44776284fd75922580299ac10c7fe7c221166
Rajarshi  Guha <rajarshi.guha@gmail.com>
2012-08-14 16:08:32 -0400

    Added a class to handle compound search via solr

---
+++ src/gov/nih/ncgc/bard/search/CompoundSearch.java
@@ -0,0 +1,62 @@
+package gov.nih.ncgc.bard.search;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.CommonsHttpSolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Full text search for compound entities.
+ *
+ * @author Rajarshi Guha
+ */
+public class CompoundSearch extends SolrSearch {
+    private final String SOLR_URL = SOLR_BASE + "/core-compound/";
+
+    Logger log;
+
+    public CompoundSearch(String query) {
+        super(query);
+        log = LoggerFactory.getLogger(this.getClass());
+    }
+
+    public void run(boolean detailed, Integer top, Integer skip) throws MalformedURLException, SolrServerException {
+        results = new SearchResult();
+
+        SolrServer solr = null;
+        solr = new CommonsHttpSolrServer(SOLR_URL);
+
+        QueryResponse response = null;
+
+        SolrQuery sq = new SolrQuery(query);
+        sq = sq.setHighlight(true).setHighlightSnippets(1);
+        if (top != null) sq = sq.setRows(top);
+        if (skip != null) sq = sq.setStart(skip);
+        if (!detailed) {
+            sq = sq.setFields("cid", "iupac_name");
+        }
+        response = solr.query(sq);
+
+        List<SolrDocument> docs = new ArrayList<SolrDocument>();
+        SolrDocumentList sdl = response.getResults();
+        for (SolrDocument doc : sdl) {
+            docs.add(doc);
+        }
+
+        SearchMeta meta = new SearchMeta();
+        meta.setNhit(sdl.getNumFound());
+
+        results.setDocs(docs);
+        results.setMetaData(meta);
+    }
+
+}
