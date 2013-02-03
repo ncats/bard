9ad204b0b4ce0633b55af5c4a7a406b02bcb205a
Rajarshi Guha <guhar@mail.nih.gov>
2013-02-03 10:16:05 -0500

    Updated validator to support plugin war

--- src/gov/nih/ncgc/bard/tools/PluginValidator.java
+++ src/gov/nih/ncgc/bard/tools/PluginValidator.java
@@ -2,14 +2,17 @@ package gov.nih.ncgc.bard.tools;
 
 import gov.nih.ncgc.bard.plugin.IPlugin;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 /**
  * A tool to validate BARD plugins.
@@ -17,6 +20,8 @@ import java.util.List;
  * @author Rajarshi Guha
  */
 public class PluginValidator {
+
+    private String currentClassName = "";
 
     private List<String> errors;
 
@@ -28,9 +33,60 @@ public class PluginValidator {
         return errors;
     }
 
-    public boolean validate(Class klass) throws IllegalAccessException, InstantiationException {
+    public class ByteArrayClassLoader extends ClassLoader {
+        byte[] bytes;
 
-        // check appropriate interface
+        public ByteArrayClassLoader(byte[] bytes) {
+            this.bytes = bytes;
+        }
+
+        public Class findClass(String name) {
+            return defineClass(name, bytes, 0, bytes.length);
+        }
+    }
+
+
+    public boolean validate(String filename) throws IOException, InstantiationException, IllegalAccessException {
+
+        String basename = (new File(filename)).getName();
+
+        boolean atLeastOnePlugin = false;
+        boolean status = false;
+
+        ZipFile zf = new ZipFile(filename);
+        Enumeration entries = zf.entries();
+        while (entries.hasMoreElements()) {
+            ZipEntry ze = (ZipEntry) entries.nextElement();
+            String entryName = ze.getName();
+            if (entryName.endsWith(".class")) {
+                BufferedInputStream bis = new BufferedInputStream(zf.getInputStream(ze));
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                int c;
+                while ((c = bis.read()) != -1) {
+                    baos.write(c);
+                }
+                bis.close();
+                baos.close();
+                byte[] bytes = baos.toByteArray();
+
+                String className = entryName.split("\\.")[0].replace("WEB-INF/classes/", "").replace("/", ".");
+                ByteArrayClassLoader loader = new ByteArrayClassLoader(bytes);
+                Class klass = loader.findClass(className);
+                if (implementsPluginInterface(klass)) {
+                    status = validate(klass, basename);
+                    atLeastOnePlugin = true;
+                }
+            }
+        }
+        zf.close();
+
+        if (!atLeastOnePlugin) {
+            errors.add("This does not seem to be a BARD plugin as there were no classes implementing the IPlugin interface");
+            return false;
+        } else return status;
+    }
+
+    private boolean implementsPluginInterface(Class klass) {
         boolean implementsInterface = false;
         Class pluginInterface = IPlugin.class;
         Class[] interfaces = klass.getInterfaces();
@@ -40,26 +96,50 @@ public class PluginValidator {
                 break;
             }
         }
+        return implementsInterface;
+    }
+
+    /**
+     * Validate a class that will expose a BARD plugin service.
+     *
+     * @param klass   The class in question.
+     * @param warName If validating via a WAR file, this should be the war file name. Otherwise, null
+     * @return true if a valid plugin class, otherwise false.
+     * @throws IllegalAccessException
+     * @throws InstantiationException
+     */
+    public boolean validate(Class klass, String warName) throws IllegalAccessException, InstantiationException {
+
+        // check appropriate interface
+        boolean implementsInterface = implementsPluginInterface(klass);
         if (!implementsInterface) {
             errors.add("Does not implement IPlugin");
         }
 
         // check that the class has a class level @Path annotation
-        // and ensure that it has "/v1/plugins" at the beginning
+        // if @Path is present, ensure it matches war file name (if we got one)
         boolean hasClassLevelPathAnnot = false;
         boolean collidesWithRegistry = false;
+        boolean matchesWarFileName = false;
         if (klass.isAnnotationPresent(Path.class)) {
+            hasClassLevelPathAnnot = true;
             Path annot = (Path) klass.getAnnotation(Path.class);
             String value = annot.value();
-            if (value != null && value.indexOf("/plugins/") == 0) {
-                hasClassLevelPathAnnot = true;
-            }
             if (value != null && value.indexOf("/plugins/registry") == 0) {
                 collidesWithRegistry = true;
             }
+            if (warName != null) {
+                String[] toks = warName.split("\\.");
+                if (toks.length == 2) {
+                    String tmp = toks[0].replace("bardplugin_","");
+                    matchesWarFileName = tmp.equals(value.replace("/",""));
+                }
+            }
         }
         if (!hasClassLevelPathAnnot)
-            errors.add("Missing the class level @Path annotation or else the annotation did not start with '/plugins/'");
+            errors.add("Missing the class level @Path annotation");
+        if (!matchesWarFileName)
+            errors.add("WAR file name does not correspond to @Path annotation");
         if (collidesWithRegistry)
             errors.add("Class level @Path annotation cannot start with '/plugins/registry'");
 
@@ -122,13 +202,12 @@ public class PluginValidator {
         return errors.size() == 0;
     }
 
-    public static void main(String[] args) throws InstantiationException, IllegalAccessException {
-//        PluginValidator v = new PluginValidator();
-//        CSLSPlugin p = new CSLSPlugin();
-//        boolean status = v.validate(p.getClass());
-//        System.out.println("status = " + status);
-//        if (!status) {
-//            for (String s : v.getErrors()) System.out.println(s);
-//        }
+    public static void main(String[] args) throws InstantiationException, IllegalAccessException, IOException {
+        PluginValidator v = new PluginValidator();
+        boolean status = v.validate("/Users/guhar/src/bard.plugins/csls/deploy/bardplugin_csls.war");
+        System.out.println("status = " + status);
+        if (!status) {
+            for (String s : v.getErrors()) System.out.println(s);
+        }
     }
 }
