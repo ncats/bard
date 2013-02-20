3901baf03241b7f1e52a76d7d450bb73e52ebb08
Rajarshi Guha <guhar@mail.nih.gov>
2013-02-19 19:00:01 -0500

    Added cap project id to probeid JSON

--- src/gov/nih/ncgc/bard/rest/BARDCompoundResource.java
+++ src/gov/nih/ncgc/bard/rest/BARDCompoundResource.java
@@ -358,8 +358,14 @@ public class BARDCompoundResource extends BARDResource<Compound> {
                         List<Project> projects = db.getProjectByProbeId(c.get(0).getProbeId());
                         ArrayNode anode = mapper.createArrayNode();
                         ObjectNode onode = mapper.valueToTree(c.get(0));
-                        if (projects.size() == 1) onode.put("bardProjectId", projects.get(0).getBardProjectId());
-                        else if (projects.size() == 0) onode.put("bardProjectId", -1);
+                        if (projects.size() == 1) {
+                            onode.put("bardProjectId", projects.get(0).getBardProjectId());
+                            onode.put("capProjectId", projects.get(0).getCapProjectId());
+                        }
+                        else if (projects.size() == 0) {
+                            onode.put("bardProjectId", -1);
+                            onode.put("capProjectId", -1);
+                        }
                         anode.add(onode);
                         json = mapper.writeValueAsString(anode);
                     } else json = Util.toJson(c);
