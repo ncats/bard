f83684b1a90bdc795e9915a2f1b4291f070b7300
John Braisted <john.braisted@nih.gov>
2013-05-07 15:05:56 -0400

    Mod to set extValueId from context item in exptdata load.

--- src/gov/nih/ncgc/bard/capextract/resultextract/BardResultFactory.java
+++ src/gov/nih/ncgc/bard/capextract/resultextract/BardResultFactory.java
@@ -255,7 +255,7 @@ public class BardResultFactory {
 	bardResult.setDisplayName(contextItem.getAttribute());
 	bardResult.setValue((contextItem.getValueNum() != null) ? contextItem.getValueNum().toString() : contextItem.getValueDisplay());
 	bardResult.setDictElemId(contextItem.getAttributeId());
-	
+	bardResult.setExtValueId(contextItem.getExtValueId());
 	return bardResult;
     }
     
@@ -267,7 +267,7 @@ public class BardResultFactory {
 	result.setDisplayName(capResult.getResultType());
 	//set value as the numeric value, or if numeric value is null, set to display value 
 	result.setValue((capResult.getValueNum() != null) ? Double.toString(capResult.getValueNum()) : capResult.getValueDisplay());
-	//take care of qualifer
+	//take care of qualifier
 	dummyStr = capResult.getQualifier();
 	if(dummyStr != null && !dummyStr.equals("="))
 	    result.setQualifierValue(capResult.getQualifier());	
