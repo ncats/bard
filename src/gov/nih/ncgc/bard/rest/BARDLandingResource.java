8e694ae92105704518b4d6bcf42f693f768d3519
Rajarshi Guha <guhar@mail.nih.gov>
2013-05-16 09:37:48 -0400

    Updated landing resources

--- src/gov/nih/ncgc/bard/rest/BARDLandingResource.java
+++ src/gov/nih/ncgc/bard/rest/BARDLandingResource.java
@@ -3,18 +3,17 @@ package gov.nih.ncgc.bard.rest;
 import gov.nih.ncgc.bard.entity.BardLinkedEntity;
 import gov.nih.ncgc.bard.tools.Util;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * A one line summary.
@@ -40,7 +39,9 @@ public class BARDLandingResource extends BARDResource {
     @GET
     @Path("/")
     public Response getResources(@QueryParam("filter") String filter, @QueryParam("expand") String expand, @QueryParam("skip") Integer skip, @QueryParam("top") Integer top) {
-        List<String> res = new ArrayList<String>(Arrays.asList(new String[]{"/assays", "/compounds", "/projects", "/experiments", "/targets", "/documents", "/substances"}));
+        List<String> res = new ArrayList<String>(Arrays.asList(new String[]{"/assays", "/biology",
+                "/compounds", "/projects", "/experiments", "/exptdata", "/cap", "/etag",
+                "/targets", "/documents", "/substances"}));
         Collections.sort(res);
         BardLinkedEntity ble = new BardLinkedEntity(res, null);
         try {
