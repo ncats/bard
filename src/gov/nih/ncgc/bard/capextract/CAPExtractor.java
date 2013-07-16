60ce3c5b9f54ff399700eb57a7715640ba92a0b6
Rajarshi Guha <guhar@mail.nih.gov>
2013-07-16 11:21:51 -0400

    Updated project handler to recognize separate probe contexts

--- src/gov/nih/ncgc/bard/capextract/CAPExtractor.java
+++ src/gov/nih/ncgc/bard/capextract/CAPExtractor.java
@@ -7,9 +7,15 @@ import gov.nih.ncgc.bard.capextract.handler.AssayHandler;
 import gov.nih.ncgc.bard.capextract.handler.AssaysHandler;
 import gov.nih.ncgc.bard.capextract.handler.BardexportHandler;
 import gov.nih.ncgc.bard.capextract.handler.DictionaryHandler;
+import gov.nih.ncgc.bard.capextract.handler.ExperimentHandler;
+import gov.nih.ncgc.bard.capextract.handler.ExperimentResultHandler;
+import gov.nih.ncgc.bard.capextract.handler.ExperimentsHandler;
 import gov.nih.ncgc.bard.capextract.handler.ExternalReferenceHandler;
+import gov.nih.ncgc.bard.capextract.handler.ExternalSystemHandler;
+import gov.nih.ncgc.bard.capextract.handler.ProjectDocHandler;
 import gov.nih.ncgc.bard.capextract.handler.ProjectHandler;
 import gov.nih.ncgc.bard.capextract.handler.ProjectsHandler;
+import gov.nih.ncgc.bard.capextract.handler.ResultHandler;
 import gov.nih.ncgc.bard.capextract.jaxb.Link;
 import gov.nih.ncgc.bard.capextract.jaxb.Projects;
 import org.slf4j.Logger;
@@ -102,23 +108,20 @@ public class CAPExtractor {
     // h.setExtractionStatus("Ready", "https://bard.broadinstitute.org/dataExport/api/assays/1587",CAPConstants.CapResource.ASSAY);
 
     public void setHandlers() {
-        AssayHandler h = new AssayHandler();
-        h.setExtractionStatus("Ready", "https://bard.broadinstitute.org/dataExport/api/assays/4463",CAPConstants.CapResource.ASSAY);
-
         registry = CapResourceHandlerRegistry.getInstance();
-//        registry.setHandler(CAPConstants.CapResource.PROJECTS, new ProjectsHandler());
-//        registry.setHandler(CAPConstants.CapResource.PROJECT, new ProjectHandler());
-//        registry.setHandler(CAPConstants.CapResource.PROJECTDOC, new ProjectDocHandler());
+        registry.setHandler(CAPConstants.CapResource.PROJECTS, new ProjectsHandler());
+        registry.setHandler(CAPConstants.CapResource.PROJECT, new ProjectHandler());
+        registry.setHandler(CAPConstants.CapResource.PROJECTDOC, new ProjectDocHandler());
         registry.setHandler(CAPConstants.CapResource.ASSAYS, new AssaysHandler());
         registry.setHandler(CAPConstants.CapResource.ASSAY, new AssayHandler());
-//        registry.setHandler(CAPConstants.CapResource.EXPERIMENTS, new ExperimentsHandler());
-//        registry.setHandler(CAPConstants.CapResource.EXPERIMENT, new ExperimentHandler());
-//        registry.setHandler(CAPConstants.CapResource.RESULT, new ResultHandler());
+        registry.setHandler(CAPConstants.CapResource.EXPERIMENTS, new ExperimentsHandler());
+        registry.setHandler(CAPConstants.CapResource.EXPERIMENT, new ExperimentHandler());
+        registry.setHandler(CAPConstants.CapResource.RESULT, new ResultHandler());
         registry.setHandler(CAPConstants.CapResource.BARDEXPORT, new BardexportHandler());
         registry.setHandler(CAPConstants.CapResource.DICTIONARY, new DictionaryHandler());
         registry.setHandler(CAPConstants.CapResource.EXTREF, new ExternalReferenceHandler());
-//        registry.setHandler(CAPConstants.CapResource.EXTSYS, new ExternalSystemHandler());
-//        registry.setHandler(CAPConstants.CapResource.RESULT_JSON, new ExperimentResultHandler());
+        registry.setHandler(CAPConstants.CapResource.EXTSYS, new ExternalSystemHandler());
+        registry.setHandler(CAPConstants.CapResource.RESULT_JSON, new ExperimentResultHandler());
 
         //        registry.setHandler(CAPConstants.CapResource.RESULTS, new ResultsHandler());
     }
@@ -181,20 +184,20 @@ public class CAPExtractor {
 	// check the load state and begin load
 	try {
 	    // returns true if state was IDLE and the lock is set and state set to LOADING, Ready to load
-//	    if(c.evaluateAndSetLoadLockState(args[0], true)) {
-        if (true) {
+	    if(c.evaluateAndSetLoadLockState(args[0], true)) {
+//        if (true) {
 		// before running the extractor, lets set our handlers
 		c.setHandlers();
 		// lets start pulling
 		c.run();	
 		// set the load state back to IDLE at the end of the load
-//		c.evaluateAndSetLoadLockState(args[0], false);
+		c.evaluateAndSetLoadLockState(args[0], false);
 	    } 
 	} catch (Exception e) {
 	    //need to set the global update time in the DB IF we fall out of the load wit error/exception!
 	    c.updateGlobalBardUpdateTime();
 	    // on any terminal error set load state to IDLE
-//	    c.evaluateAndSetLoadLockState(args[0], false);
+	    c.evaluateAndSetLoadLockState(args[0], false);
 	    e.printStackTrace();
 	}
 
