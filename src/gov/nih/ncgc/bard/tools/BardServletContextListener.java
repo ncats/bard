a92bd1a591db675e0d3ab39a6d035c877ee23bde
Rajarshi Guha <guhar@mail.nih.gov>
2013-09-27 16:00:11 -0400

    Modified cache mgmt init so that we can skip it via cmd line parameter Get an instance of DBUtils at servlet load time Updated base resource for REST resources to have a static DButils field

--- src/gov/nih/ncgc/bard/tools/BardServletContextListener.java
+++ src/gov/nih/ncgc/bard/tools/BardServletContextListener.java
@@ -1,51 +1,64 @@
 package gov.nih.ncgc.bard.tools;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import gov.nih.ncgc.bard.rest.BARDResource;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.sql.SQLException;
 
 /**
  * This class is specified in the container configuration file (web.xml) as a registered listener.
  * Methods handle initialization and destruction of application context.
- * 
- * @author braistedjc
  *
+ * @author braistedjc
  */
 public class BardServletContextListener implements ServletContextListener {
+    DBUtils db;
 
     @Override
     public void contextInitialized(ServletContextEvent contextEvent) {
-	// initialize cache management parameters
-	ServletContext servletContext = contextEvent.getServletContext();
-	String cachePrefixes = servletContext.getInitParameter("cache-management-cache-prefix-list");
-	String cacheMgrNodes = servletContext.getInitParameter("cache-manager-cluster-nodes");
-	if(cachePrefixes != null && cacheMgrNodes != null) {
-	    DBUtils.initializeManagedCaches(cachePrefixes, cacheMgrNodes);
-	} else {
-	    // if we don't have context, then try to initialize with the assumption that the
-	    // server host is a member of the HazelCast cluster, use the host ip or if that fails, localhost
-	    try {
-		String host = InetAddress.getLocalHost().getHostAddress();
-		//if we don't have context, send empty cache prefix list, 
-		DBUtils.initializeManagedCaches("", host);
-	    } catch (UnknownHostException unknownHostExcept) {
-		unknownHostExcept.printStackTrace();
-		DBUtils.initializeManagedCaches("", "localhost");
-	    }	    
-	}
+        db = new DBUtils();
+        BARDResource.setDb(db);
 
-	// additional initialization can go here ...
+        // should we initialize cache mgmt at all?
+        boolean initHazelcast = System.getProperty("initHazelcast").toLowerCase().equals("true");
+
+        // initialize cache management parameters
+        if (initHazelcast) {
+            ServletContext servletContext = contextEvent.getServletContext();
+            String cachePrefixes = servletContext.getInitParameter("cache-management-cache-prefix-list");
+            String cacheMgrNodes = servletContext.getInitParameter("cache-manager-cluster-nodes");
+            if (cachePrefixes != null && cacheMgrNodes != null) {
+                DBUtils.initializeManagedCaches(cachePrefixes, cacheMgrNodes);
+            } else {
+                // if we don't have context, then try to initialize with the assumption that the
+                // server host is a member of the HazelCast cluster, use the host ip or if that fails, localhost
+                try {
+                    String host = InetAddress.getLocalHost().getHostAddress();
+                    //if we don't have context, send empty cache prefix list,
+                    DBUtils.initializeManagedCaches("", host);
+                } catch (UnknownHostException unknownHostExcept) {
+                    unknownHostExcept.printStackTrace();
+                    DBUtils.initializeManagedCaches("", "localhost");
+                }
+            }
+        }
+
+        // additional initialization can go here ...
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent arg0) {
-	//closes the cache manger
-	DBUtils.shutdownCacheFlushManager();
-	DBUtils.flushCachePrefixNames = null;
-	
-	// additional context shutdown/cleanup can go here ... 
+        //closes the cache manger
+        DBUtils.shutdownCacheFlushManager();
+        DBUtils.flushCachePrefixNames = null;
+        try {
+            db.closeConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
     }
 }
