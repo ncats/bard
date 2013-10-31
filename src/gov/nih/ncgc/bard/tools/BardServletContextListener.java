c1b4ad45e303ac967cefa2259e7a33885560f700
Rajarshi Guha <guhar@mail.nih.gov>
2013-10-31 10:18:28 -0400

    update hazel cast init. people need to move to v18

--- src/gov/nih/ncgc/bard/tools/BardServletContextListener.java
+++ src/gov/nih/ncgc/bard/tools/BardServletContextListener.java
@@ -1,51 +1,56 @@
 package gov.nih.ncgc.bard.tools;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * This class is specified in the container configuration file (web.xml) as a registered listener.
  * Methods handle initialization and destruction of application context.
- * 
- * @author braistedjc
  *
+ * @author braistedjc
  */
 public class BardServletContextListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent contextEvent) {
-	// initialize cache management parameters
-	ServletContext servletContext = contextEvent.getServletContext();
-	String cachePrefixes = servletContext.getInitParameter("cache-management-cache-prefix-list");
-	String cacheMgrNodes = servletContext.getInitParameter("cache-manager-cluster-nodes");
-	if(cachePrefixes != null && cacheMgrNodes != null) {
-	    DBUtils.initializeManagedCaches(cachePrefixes, cacheMgrNodes);
-	} else {
-	    // if we don't have context, then try to initialize with the assumption that the
-	    // server host is a member of the HazelCast cluster, use the host ip or if that fails, localhost
-	    try {
-		String host = InetAddress.getLocalHost().getHostAddress();
-		//if we don't have context, send empty cache prefix list, 
-		DBUtils.initializeManagedCaches("", host);
-	    } catch (UnknownHostException unknownHostExcept) {
-		unknownHostExcept.printStackTrace();
-		DBUtils.initializeManagedCaches("", "localhost");
-	    }	    
-	}
+        String sym = System.getProperty("initHazelcast");
+        boolean initHazelcast = true;
+        if (sym != null) initHazelcast = sym.toLowerCase().equals("true");
 
-	// additional initialization can go here ...
+
+        // initialize cache management parameters
+        if (initHazelcast) {
+            ServletContext servletContext = contextEvent.getServletContext();
+            String cachePrefixes = servletContext.getInitParameter("cache-management-cache-prefix-list");
+            String cacheMgrNodes = servletContext.getInitParameter("cache-manager-cluster-nodes");
+            if (cachePrefixes != null && cacheMgrNodes != null) {
+                DBUtils.initializeManagedCaches(cachePrefixes, cacheMgrNodes);
+            } else {
+                // if we don't have context, then try to initialize with the assumption that the
+                // server host is a member of the HazelCast cluster, use the host ip or if that fails, localhost
+                try {
+                    String host = InetAddress.getLocalHost().getHostAddress();
+                    //if we don't have context, send empty cache prefix list,
+                    DBUtils.initializeManagedCaches("", host);
+                } catch (UnknownHostException unknownHostExcept) {
+                    unknownHostExcept.printStackTrace();
+                    DBUtils.initializeManagedCaches("", "localhost");
+                }
+            }
+        }
+
+        // additional initialization can go here ...
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent arg0) {
-	//closes the cache manger
-	DBUtils.shutdownCacheFlushManager();
-	DBUtils.flushCachePrefixNames = null;
-	
-	// additional context shutdown/cleanup can go here ... 
+        //closes the cache manger
+        DBUtils.shutdownCacheFlushManager();
+        DBUtils.flushCachePrefixNames = null;
+
+        // additional context shutdown/cleanup can go here ...
     }
 }
