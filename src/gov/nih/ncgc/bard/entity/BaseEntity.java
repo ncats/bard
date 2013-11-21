896716bd677ac3588a4b1fbdd952afac884533cc
Rajarshi Guha <guhar@mail.nih.gov>
2013-11-21 15:39:03 -0500

    remove status warning from entities

--- src/gov/nih/ncgc/bard/entity/BaseEntity.java
+++ src/gov/nih/ncgc/bard/entity/BaseEntity.java
@@ -1,20 +1,7 @@
 package gov.nih.ncgc.bard.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 /**
  * @author Rajarshi Guha
  */
 public abstract class BaseEntity implements BardEntity {
-
-    @JsonIgnore
-    public String statusWarning = "THIS IS PREVIEW DATA that has not been curated yet, and may be revised or deprecated without notice. PLEASE USE WITH CAUTION";
-
-    public String getStatusWarning() {
-        return statusWarning;
-    }
-
-    public void setStatusWarning(String statusWarning) {
-        this.statusWarning = statusWarning;
-    }
 }
