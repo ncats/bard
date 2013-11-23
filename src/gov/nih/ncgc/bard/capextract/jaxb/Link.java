db4bc09d18da72991672502d4a7d6abbeca199f5
Rajarshi Guha <guhar@mail.nih.gov>
2013-11-23 11:50:13 -0500

    Added in fix to JAXB classes

--- src/gov/nih/ncgc/bard/capextract/jaxb/Link.java
+++ src/gov/nih/ncgc/bard/capextract/jaxb/Link.java
@@ -14,6 +14,7 @@ import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlSchemaType;
 import javax.xml.bind.annotation.XmlType;
+import java.io.Serializable;
 
 
 /**
@@ -76,7 +77,7 @@ import javax.xml.bind.annotation.XmlType;
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "")
 @XmlRootElement(name = "link")
-public class Link {
+public class Link implements Serializable {
 
     @XmlAttribute(name = "type", required = true)
     protected String type;
